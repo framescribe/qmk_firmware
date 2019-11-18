4961bd10199b53de413f0153b45efb5303b1c090
Justin Rhodes <justin@almighty.ninja>
2019-11-18 14:31:09 -0800

    atreus 62

---
+++ keyboards/atreus62/keymaps/justin/config.h
@@ -0,0 +1,59 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+
+
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 200
+#define LEADER_TIMEOUT 350
+
+
+
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#define AUTO_SHIFT_TIMEOUT 175
+#define NO_AUTO_SHIFT_SPECIAL
+#define NO_AUTO_SHIFT_ALPHA
+
+#endif
+
+#define MOUSEKEY_INTERVAL       20
+#define MOUSEKEY_DELAY          0
+#define MOUSEKEY_TIME_TO_MAX    60
+#define MOUSEKEY_MAX_SPEED      7
+
+#define MOUSEKEY_WHEEL_MAX_SPEED 2
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 250
+#define MK_W_INTERVAL_UNMOD 200
+#define MK_W_OFFSET_UNMOD 1
+#define MOUSEKEY_WHEEL_DELAY        300
+
+#ifdef AUDIO_ENABLE
+  #define STARTUP_SONG SONG(PLANCK_SOUND)
+#endif
