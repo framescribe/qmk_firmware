31d302bde5b8fe5e4b9e8c71e735be48c85de569
Justin Rhodes <justin@almighty.ninja>
2019-11-18 15:55:05 -0800

    kyria no space cadet

--- keyboards/kyria/keymaps/justin2/keymap.c
+++ keyboards/kyria/keymaps/justin2/keymap.c
@@ -126,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
       LT(5, KC_TAB),   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
       KC_LEAD,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                   KC_H,    KC_J,    KC_K,    KC_L,    ARRCLN,  KC_QUOT,
-      KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   _______,   KC_LEAD, KC_LEAD, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+      KC_LSPO,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   _______,   KC_LEAD, KC_LEAD, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
                                 PASTE,  OPT,    CMD,    BSPC,      LWORD,   RWORD,   SPC,    ENTL1,   MO(2),  KC_MUTE
     ),
 /*
