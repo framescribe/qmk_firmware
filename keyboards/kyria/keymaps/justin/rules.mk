df3b0718e932538286ebc570d2c8f59e85fcf080
Justin Rhodes <justin@almighty.ninja>
2019-11-21 15:10:15 -0800

    update

---
+++ keyboards/kyria/keymaps/justin/rules.mk
@@ -0,0 +1,7 @@
+OLED_DRIVER_ENABLE 		= yes   # Enables the use of OLED displays
+ENCODER_ENABLE 			= yes   # ENables the use of one or more encoders
+RGBLIGHT_ENABLE 		= yes   # Enable keyboard RGB underglow
+LEADER_ENABLE 			= yes	# Enable leader key
+MOUSEKEY_ENABLE         = yes   # Mouse keys(+4700)
+EXTRAKEY_ENABLE         = yes   # Audio control and System control(+450)
+NKRO_ENABLE             = yes   # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
